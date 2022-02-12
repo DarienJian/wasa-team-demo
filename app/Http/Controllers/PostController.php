8bcaf85134147a91401f0921a2633b86487b028d
Darien <r880514@gmail.com>
2022-02-12 18:46:54 +0800

    建立文章crud

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Posts\StorePostRequest;
+use App\Http\Requests\Posts\UpdatePostRequest;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return  Post::all();
+    }
+
+    public function store(StorePostRequest $request)
+    {
+        $validated = $request->validated();
+        return Post::create($validated);
+    }
+
+    public function show(Post $post)
+    {
+        return $post;
+    }
+
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        $validated = $request->validated();
+        $post->update($validated);
+
+        return $post;
+    }
+
+    public function destroy(Post $post)
+    {
+        return $post->delete();
+    }
+}
